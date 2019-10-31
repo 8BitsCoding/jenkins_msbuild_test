44338949d6b00647b5cee08746aaaf4ddac28aab
kimtaehyung <taehyeong77@naver.com>
2019-10-31 13:58:10 +0900

    commit

---
+++ Jenkins_MSbuild_MFC/Jenkins_MSbuild_MFC/Jenkins_MSbuild_MFC.cpp
@@ -0,0 +1,46 @@
+﻿
+// Jenkins_MSbuild_MFC.cpp: 응용 프로그램에 대한 클래스 동작을 정의합니다.
+//
+
+#include "stdafx.h"
+#include "Jenkins_MSbuild_MFC.h"
+#include "Jenkins_MSbuild_MFCDlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CJenkinsMSbuildMFCApp
+
+BEGIN_MESSAGE_MAP(CJenkinsMSbuildMFCApp, CWinApp)
+	ON_COMMAND(ID_HELP, &CWinApp::OnHelp)
+END_MESSAGE_MAP()
+
+
+// CJenkinsMSbuildMFCApp 생성
+
+CJenkinsMSbuildMFCApp::CJenkinsMSbuildMFCApp()
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+	// InitInstance에 모든 중요한 초기화 작업을 배치합니다.
+}
+
+
+// 유일한 CJenkinsMSbuildMFCApp 개체입니다.
+
+CJenkinsMSbuildMFCApp theApp;
+
+
+// CJenkinsMSbuildMFCApp 초기화
+
+BOOL CJenkinsMSbuildMFCApp::InitInstance()
+{
+
+	CJenkinsMSbuildMFCDlg dlg;
+	m_pMainWnd = &dlg;
+	INT_PTR nResponse = dlg.DoModal();
+
+	return FALSE;
+}
+
---
+++ Jenkins_MSbuild_MFC/Jenkins_MSbuild_MFC/Jenkins_MSbuild_MFCDlg.h
@@ -0,0 +1,33 @@
+﻿
+// Jenkins_MSbuild_MFCDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CJenkinsMSbuildMFCDlg 대화 상자
+class CJenkinsMSbuildMFCDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CJenkinsMSbuildMFCDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_JENKINS_MSBUILD_MFC_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
